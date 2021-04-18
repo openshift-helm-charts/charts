19922d5c7716e5fcb29203cbbabbcad564372602
Baiju Muthukadan <baiju.m.mail@gmail.com>
2021-04-18 19:57:55 +0530

    Print stdout & stderr

--- scripts/src/chartrepomanager/chartrepomanager.py
+++ scripts/src/chartrepomanager/chartrepomanager.py
@@ -16,8 +16,12 @@ except ImportError:
 
 def get_modified_charts():
     commit = subprocess.run(["git", "rev-parse", "--verify", "HEAD"], capture_output=True)
+    print(commit.stdout.decode("utf-8"))
+    print(commit.stderr.decode("utf-8"))
     commit_hash = commit.stdout.strip()
     files = subprocess.run(["git", "diff-tree", "--no-commit-id", "--name-only", "-r", commit_hash], capture_output=True)
+    print(files.stdout.decode("utf-8"))
+    print(files.stderr.decode("utf-8"))
     pattern = re.compile("charts/(\w+)/([\w-]+)/([\w-]+)/([\w\.]+)/.*")
     count = 0
     for line in files.stdout.decode("utf-8").split('\n'):
@@ -30,6 +34,8 @@ def get_modified_charts():
 def prepare_chart_for_release(category, organization, chart, version):
     path = os.path.join("charts", category, organization, chart, version, "src")
     out = subprocess.run(["helm", "package", path], capture_output=True)
+    print(out.stdout.decode("utf-8"))
+    print(out.stderr.decode("utf-8"))
     p = out.stdout.decode("utf-8").strip().split(":")[1].strip()
     chartname = os.path.basename(p)
     try:
@@ -42,11 +48,15 @@ def prepare_chart_for_release(category, organization, chart, version):
 def push_chart_release():
     token = os.environ.get("GITHUB_TOKEN")
     if token:
-        subprocess.run(["cr", "upload", "-o", "openshift-helm-charts", "-r", "repo", "-t", token], capture_output=True)
+        out = subprocess.run(["cr", "upload", "-o", "openshift-helm-charts", "-r", "repo", "-t", token], capture_output=True)
+        print(out.stdout.decode("utf-8"))
+        print(out.stderr.decode("utf-8"))
 
 def create_worktree_for_index():
     dr = tempfile.mkdtemp(prefix="crm-")
-    subprocess.run(["git", "worktree", "add", "--detach", dr, "origin/gh-pages"], capture_output=True)
+    out = subprocess.run(["git", "worktree", "add", "--detach", dr, "origin/gh-pages"], capture_output=True)
+    print(out.stdout.decode("utf-8"))
+    print(out.stderr.decode("utf-8"))
     return dr
 
 def create_index(indexdir, branch, chartname, category, organization, chart, version):
@@ -62,6 +72,8 @@ def create_index(indexdir, branch, chartname, category, organization, chart, ver
     if os.path.exists(os.path.join(path, "src")):
         out = subprocess.run(["helm", "show", "chart", os.path.join(path, "src")], capture_output=True)
         p = out.stdout.decode("utf-8")
+        print(p)
+        print(out.stderr.decode("utf-8"))
         crt = yaml.load(p, Loader=Loader)
 
     crtentries = []
@@ -77,11 +89,19 @@ def create_index(indexdir, branch, chartname, category, organization, chart, ver
     with open(os.path.join(indexdir, "index.yaml"), "w") as fd:
         fd.write(out)
     out = subprocess.run(["git", "add", os.path.join(indexdir, "index.yaml")], cwd=indexdir, capture_output=True)
+    print(out.stdout.decode("utf-8"))
+    print(out.stderr.decode("utf-8"))
     out = subprocess.run(["git", "commit", indexdir, "-m", "Update index.html"], cwd=indexdir, capture_output=True)
+    print(out.stdout.decode("utf-8"))
+    print(out.stderr.decode("utf-8"))
     out = subprocess.run(["git", "push", f"https://x-access-token:{token}@github.com/openshift-helm-charts/repo", "HEAD:refs/heads/"+branch, "-f"], cwd=indexdir, capture_output=True)
+    print(out.stdout.decode("utf-8"))
+    print(out.stderr.decode("utf-8"))
 
 def update_chart_annotation(chartname):
-    subprocess.run(["tar", "zxvf", os.path.join(".cr-release-packages", chartname)], capture_output=True)
+    out = subprocess.run(["tar", "zxvf", os.path.join(".cr-release-packages", chartname)], capture_output=True)
+    print(out.stdout.decode("utf-8"))
+    print(out.stderr.decode("utf-8"))
 
 def main():
     parser = argparse.ArgumentParser()
