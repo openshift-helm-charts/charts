f4a12b8beb2cfc43b59c4eb2b094b9be1fd23609
Baiju Muthukadan <baiju.m.mail@gmail.com>
2021-05-01 22:52:37 +0530

    Add link to error log directly in the comment
    Fixes: https://issues.redhat.com/browse/HELM-155

--- scripts/prepare_pr_comment.py
+++ scripts/prepare_pr_comment.py
@@ -1,8 +1,8 @@
 import sys
 
-def prepare_failure_commet(issue_number, vendor_label, chart_name):
+def prepare_failure_commet(repository, issue_number, vendor_label, chart_name):
     msg = f"Thank you for the PR #{issue_number}!\n\n"
-    msg += "There are some error while building validating your PR. Please check the log for more details.\n\n"
+    msg += f"There are [some errors](https://github.com/{repository}/pull/{issue_number}/checks) while building and validating your PR. Please check the log for more details.\n\n"
     msg += f'/metadata {{"vendor_label": "{vendor_label}", "chart_name": "{chart_name}"}}\n\n'
     msg += f"Partner Support URL: https://redhat-connect.gitbook.io/red-hat-partner-connect-general-guide/managing-your-account/getting-help/technology-partner-success-desk\n"
     return msg
@@ -14,11 +14,12 @@ def prepare_success_commet(issue_number, vendor_label, chart_name):
 
 def main():
     result = sys.argv[1]
+    repository = sys.argv[2]
     issue_number = open("./pr/NR").read().strip()
     vendor_label = open("./pr/vendor").read().strip()
     chart_name = open("./pr/chart").read().strip()
     if result == "failure":
-        msg = prepare_failure_commet(issue_number, vendor_label, chart_name)
+        msg = prepare_failure_commet(repository, issue_number, vendor_label, chart_name)
     else:
         msg = prepare_success_commet(issue_number, vendor_label, chart_name)
 
