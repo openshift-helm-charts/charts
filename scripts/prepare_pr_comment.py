f829f0b68fe5aefc64b57ff98023504f542e568f
Baiju Muthukadan <baiju.m.mail@gmail.com>
2021-05-02 10:35:52 +0530

    Display error details, if present
    Also, add a link to the error log.
    
    This is a follow-up PR on #69

--- scripts/prepare_pr_comment.py
+++ scripts/prepare_pr_comment.py
@@ -1,8 +1,18 @@
+import os
 import sys
 
 def prepare_failure_commet(repository, issue_number, vendor_label, chart_name):
     msg = f"Thank you for the PR #{issue_number}!\n\n"
-    msg += f"There are [some errors](https://github.com/{repository}/pull/{issue_number}/checks) while building and validating your PR. Please check the log for more details.\n\n"
+    runid = open("./pr/build-verify-check-run-id").read()
+    run_url = f"https://github.com/{repository}/pull/{issue_number}/checks?check_run_id={runid}".strip()
+    msg += f'There are some errors while building and validating your PR.'
+    msg += 'Please check the log for more details:\n'
+    msg += run_url
+    msg += '\n(Please open this link in another tab/window)\n\n'
+    if os.path.exists("./pr/errors"):
+        msg += f"These are the critical issues:\n\n"
+        msg += open("./pr/errors").read()
+    msg += "\n---\n"
     msg += f'/metadata {{"vendor_label": "{vendor_label}", "chart_name": "{chart_name}"}}\n\n'
     msg += f"Partner Support URL: https://redhat-connect.gitbook.io/red-hat-partner-connect-general-guide/managing-your-account/getting-help/technology-partner-success-desk\n"
     return msg
