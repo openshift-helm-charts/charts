996a35bc42dddf6e89732b7ebdaf55e6b3bbf45f
Martin Mulholland <mmulholl@redhat.com>
2021-05-13 17:59:30 -0400

    Do not set up for testing of report exists

--- scripts/src/sanitycheckpr/sanitycheckpr.py
+++ scripts/src/sanitycheckpr/sanitycheckpr.py
@@ -18,11 +18,15 @@ def ensure_only_chart_is_modified(api_url):
     headers = {'Accept': 'application/vnd.github.v3+json'}
     r = requests.get(files_api_url, headers=headers)
     pattern = re.compile(r"charts/(\w+)/([\w-]+)/([\w-]+)/([\w\.]+)/.*")
+    reportpattern = re.compile(r"charts/(\w+)/([\w-]+)/([\w-]+)/([\w\.]+)/report.yaml")
     count = 0
     for f in r.json():
         if not pattern.match(f["filename"]):
             print("PR should only modify chart related files")
             sys.exit(1)
+        elif reportpattern.match(f["filename"]):
+            print("[INFO] Report found")
+            print("::set-output name=report-exists::true")
 
 def main():
     parser = argparse.ArgumentParser()
