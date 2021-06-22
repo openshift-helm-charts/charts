909894bec75b14f7d2c87fc34292aa4669a050a9
Baiju Muthukadan <baiju.m.mail@gmail.com>
2021-06-22 21:41:37 +0530

    Retry PR merge status check (#205)

--- scripts/src/checkautomerge/checkautomerge.py
+++ scripts/src/checkautomerge/checkautomerge.py
@@ -1,5 +1,4 @@
-import re
-import os
+import time
 import sys
 import argparse
 
@@ -8,8 +7,15 @@ import requests
 def ensure_pull_request_not_merged(api_url):
     # api_url https://api.github.com/repos/<organization-name>/<repository-name>/pulls/1
     headers = {'Accept': 'application/vnd.github.v3+json'}
-    r = requests.get(api_url, headers=headers)
-    if not r.json()["merged"]:
+    merged = False
+    for i in range(20):
+        r = requests.get(api_url, headers=headers)
+        if r.json()["merged"]:
+            merged = True
+            break
+        time.sleep(10)
+
+    if not merged:
         print("[ERROR] Pull request not merged")
         sys.exit(1)
 
