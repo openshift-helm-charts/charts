af1995069ff3a12b08141908baed4a3f859a33f4
Allen Bai <abai@redhat.com>
2021-08-04 12:06:23 -0400

    tests: fix chart src testing when src dir already exists
    When there's a src directory under the target location under charts/,
         the test case will fail because the code will move the test chart
         into the src directory, which will result in mismatched SHA value.
    
    This fixes the issue by removing the src directory in the runner and
    test with the test charts.
    
    Signed-off-by: Allen Bai <abai@redhat.com>

--- tests/functional/test_chart_src_without_report.py
+++ tests/functional/test_chart_src_without_report.py
@@ -232,10 +232,16 @@ def the_user_has_created_a_error_free_chart_src(secrets):
                       f'HEAD:refs/heads/{secrets.base_branch}', '-f')
 
         # Unzip files into temporary directory for PR submission
-        with tarfile.open(secrets.test_chart, 'r') as fd:
-            fd.extractall(f'{chart_dir}/{secrets.chart_version}')
-            shutil.move(f'{chart_dir}/{secrets.chart_version}/{secrets.chart_name}',
-                        f'{chart_dir}/{secrets.chart_version}/src')
+        try:
+            logger.info(f"Remove existing local '{chart_dir}/{secrets.chart_version}/src'")
+            shutil.rmtree(f'{chart_dir}/{secrets.chart_version}/src')
+        except FileNotFoundError:
+            logger.info(f"'{chart_dir}/{secrets.chart_version}/src' does not exist")
+        finally:
+            with tarfile.open(secrets.test_chart, 'r') as fd:
+                fd.extractall(f'{chart_dir}/{secrets.chart_version}')
+                os.rename(f'{chart_dir}/{secrets.chart_version}/{secrets.chart_name}',
+                            f'{chart_dir}/{secrets.chart_version}/src')
 
         # Push chart src files to test_repo:pr_branch
         repo.git.add(f'{chart_dir}/{secrets.chart_version}/src')
