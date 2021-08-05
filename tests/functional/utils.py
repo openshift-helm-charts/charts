64c05fadc955fcdd4d5b6208f4641036abdc163d
Allen Bai <abai@redhat.com>
2021-08-05 11:14:43 -0400

    tests: use bot sandbox repo for testing
    Signed-off-by: Allen Bai <abai@redhat.com>

--- tests/functional/utils.py
+++ tests/functional/utils.py
@@ -13,7 +13,7 @@ from retrying import retry
 
 GITHUB_BASE_URL = 'https://api.github.com'
 # The sandbox repository where we run all our tests on
-TEST_REPO = 'openshift-helm-charts/sandbox'
+TEST_REPO = 'openshift-helm-charts-bot/sandbox'
 # The prod repository where we create notification issues
 PROD_REPO = 'openshift-helm-charts/charts'
 # The prod branch where we store all chart files
