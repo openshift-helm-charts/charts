d6a7136c50f6942e7a6c35d5ecb6b95bd2aed4d1
David Peraza <dperaza@dustbit.com>
2021-09-15 14:52:25 -0400

    Fixing semver regular expresion (#330)

--- scripts/src/chartrepomanager/chartrepomanager.py
+++ scripts/src/chartrepomanager/chartrepomanager.py
@@ -24,7 +24,7 @@ def get_modified_charts(api_url):
     files_api_url = f'{api_url}/files'
     headers = {'Accept': 'application/vnd.github.v3+json'}
     r = requests.get(files_api_url, headers=headers)
-    pattern = re.compile(r"charts/(\w+)/([\w-]+)/([\w-]+)/([\w\.]+)/.*")
+    pattern = re.compile(r"charts/(\w+)/([\w-]+)/([\w-]+)/([\w\.-]+)/.*")
     for f in r.json():
         m = pattern.match(f["filename"])
         if m:
