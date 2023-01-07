eeef9b500d5f24cfeebaba6bd64169e7c2b1f1cc
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-07 18:30:52 +0000

    Adding tarfile member sanitization to extractall()

--- tests/functional/behave_features/common/utils/chart.py
+++ tests/functional/behave_features/common/utils/chart.py
@@ -122,7 +122,26 @@ def extract_chart_tgz(src, dst, chart_name, logger):
         logger.info(f"'{dst}/src' does not exist")
     finally:
         with tarfile.open(src, 'r') as fd:
-            fd.extractall(dst)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(fd, dst)
             os.rename(f'{dst}/{chart_name}', f'{dst}/src')
 
 def get_all_charts(charts_path: str, vendor_types: str) -> list:
