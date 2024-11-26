d82d54ef664a9384b1661daa7521b67b9a0a7162
RHDH Build (rhdh-bot) <rhdh-bot@redhat.com>
2024-11-26 12:26:30 -0500

    chore: add redhat-developer-hub-1.4-105-CI
    Signed-off-by: RHDH Build (rhdh-bot) <rhdh-bot@redhat.com>

---
+++ installation/README.md
@@ -0,0 +1,52 @@
+
+## Scripted installation
+
+To [install](./install.sh) from a Helm Chart Repository, run the following commands:
+
+```
+cd /tmp
+# Create or select a namespace
+# Install the chart repo
+# Install the chart, then update the clusterRouterBase
+curl -sSLO https://raw.githubusercontent.com/rhdh-bot/openshift-helm-charts/redhat-developer-hub-1.4-105-CI/installation/install.sh && chmod +x install.sh
+./install.sh 1.4-105-CI --namespace rhdh-1-4-105-ci --chartrepo
+```
+
+That's it! 
+
+
+## Manual installation
+
+The [install](./install.sh) script creates a chart repo, then follows the [standard installation guide](https://access.redhat.com/documentation/en-us/red_hat_developer_hub/1.1/html-single/administration_guide_for_red_hat_developer_hub/index#proc-install-rhdh-helm_admin-rhdh) and automates these steps:
+
+1. Create a chart repo, with .metadata.name = `rhdh-next-ci-repo`
+```
+oc apply -f https://github.com/rhdh-bot/openshift-helm-charts/raw/redhat-developer-hub-1.4-105-CI/installation/rhdh-next-ci-repo.yaml
+```
+2. Go to `Developer` perspective in your cluster
+1. Select your namespace or project (eg., `rhdh-helm` or `rhdh-1-4-105-ci`)
+1. Click `+Add`, scroll down and select `Helm Chart`
+1. Filter out the default charts and just select the `Rhdh Next Ci Repo`
+1. **IMPORTANT**: In the chart's YAML view, change the following line to the correct value for your cluster. For example, change
+```
+clusterRouterBase: apps.example.com
+```
+to
+```
+clusterRouterBase: apps.ci-my-cluster-goes-here.com
+```
+7. Click `Create` and watch the deployment happen from the `Topology` view.
+1. Open the `Route` once it's available to see your deployed RHDH instance.
+
+## Optional Verification
+
+To verify a chart, use chart-verifier. This is only needed if you built your own chart and want to check it passes compliance checks.
+
+```
+cd /tmp && mkdir -p chartverifier; \\
+podman run --rm -i -e KUBECONFIG=/.kube/config \\
+  -v /home/nboldt/.kube:/.kube:z -v /tmp/chartverifier:/app/chartverifier:z \\
+  quay.io/redhat-certification/chart-verifier \\
+  verify --write-to-file https://github.com/rhdh-bot/openshift-helm-charts/raw/redhat-developer-hub-1.4-105-CI/charts/redhat/redhat/redhat-developer-hub/1.4-105-CI/redhat-developer-hub-1.4-105-CI.tgz
+echo 'Report in /tmp/chartverifier/report.yaml'
+```  
---
+++ installation/install.sh
@@ -0,0 +1,77 @@
+#!/bin/bash  -e
+#
+# Copyright (c) 2024 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# install a helm chart with the correct global.clusterRouterBase
+
+# default namespace if none set
+namespace="rhdh-helm"
+chartrepo=0 # by default don't create a new chart repo unless the version chart version includes "CI" suffix
+
+usage ()
+{
+  echo "Usage: $0 CHART_VERSION [-n namespace]
+
+Examples:
+  $0 1.1.1 
+  $0 1.4-105-CI -n rhdh-ci
+
+Options:
+  -n, --namespace   Project or namespace into which to install specified chart; default: $namespace
+  -r, --chartrepo   If set, a Helm Chart Repo will be applied to the cluster, based on the chart version.
+                    If CHART_VERSION ends in CI, this is done by default.
+"
+  exit
+}
+
+if [[ $# -lt 1 ]]; then usage; fi
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    '-r'|'--chartrepo') chartrepo=1;;
+    '-n'|'--namespace') namespace="$2"; shift 1;;
+    '-h') usage;;
+    *) CV="$1";;
+  esac
+  shift 1
+done
+
+if [[ ! $CV ]]; then usage; fi
+
+
+tmpfile=/tmp/redhat-developer-hub.chart.values.yml
+CHART_URL="https://github.com/rhdh-bot/openshift-helm-charts/raw/redhat-developer-hub-${CV}/charts/redhat/redhat/redhat-developer-hub/${CV}/redhat-developer-hub-${CV}.tgz"
+
+# choose namespace for the install (or create if non-existant)
+oc new-project "$namespace" || oc project "$namespace"
+
+# if a CI chart, create a chart repo
+if [[ $CV == *"-CI" ]]; then chartrepo=1; fi
+if [[ $chartrepo -eq 1 ]]; then
+    oc apply -f https://github.com/rhdh-bot/openshift-helm-charts/raw/redhat-developer-hub-${CV}/installation/rhdh-next-ci-repo.yaml
+fi
+
+# 1. install (or upgrade)
+helm upgrade redhat-developer-hub -i "${CHART_URL}"
+
+# 2. collect values
+PASSWORD=$(kubectl get secret redhat-developer-hub-postgresql -o jsonpath="{.data.password}" | base64 -d)
+CLUSTER_ROUTER_BASE=$(oc get route console -n openshift-console -o=jsonpath='{.spec.host}' | sed 's/^[^.]*\.//')
+
+# 3. change values
+helm upgrade redhat-developer-hub -i "${CHART_URL}" \
+    --set global.clusterRouterBase="${CLUSTER_ROUTER_BASE}" \
+    --set global.postgresql.auth.password="$PASSWORD"
+
+# 4. cleanup
+rm -f "$tmpfile"
+
+echo "
+Once deployed, Developer Hub $CV will be available at
+https://redhat-developer-hub-${namespace}.${CLUSTER_ROUTER_BASE}
+"
