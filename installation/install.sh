3ae8cc77b2781b2ab9475e35e649f7645f98df33
RHDH Build (rhdh-bot) <rhdh-bot@redhat.com>
2025-03-17 14:44:05 +0000

    chore: add redhat-developer-hub-1.6-51-CI
    Signed-off-by: RHDH Build (rhdh-bot) <rhdh-bot@redhat.com>

--- installation/install.sh
+++ installation/install.sh
@@ -20,7 +20,7 @@ usage ()
 
 Examples:
   $0 1.1.1 
-  $0 1.6-50-CI -n rhdh-ci
+  $0 1.6-51-CI -n rhdh-ci
 
 Options:
   -n, --namespace   Project or namespace into which to install specified chart; default: $namespace
